2f2ec43eee12f3778bea37ff5f475b99e217ce9d
Jenn Le <jenn.thakugan@icloud.com>
2018-09-22 22:00:57 -0500

    Added make_move, need to add tests and should prob refactor the pattern matching

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -1,10 +1,10 @@
 defmodule Hangman.Game do
   defmodule State do
     defstruct(
-      game_state: :initializing,
-      turns_left: 7,
-      letters: [],
-      used: [],
+      game_state:   :initializing,
+      turns_left:   7,
+      letters:      [],
+      used:         [],
       last_guessed: ""
     )
   end
@@ -18,11 +18,19 @@ defmodule Hangman.Game do
 
   # Returns the tally for the given game
   def tally(game = %Hangman.Game.State{}) do
-    letters =
-      game.letters
-      |> tally_letters(game.used)
+    letters = game.letters |> tally_letters(game.used)
 
     %Hangman.Game.State{ game | letters: letters }
+  end
+
+  # Returns a tuple containing the updated game state
+  # and a tally
+  def make_move(game = %Hangman.Game.State{}, guess) do
+    updated_state = game
+      |> update_guesses(guess)
+      |> update_status(game.used)
+
+    { updated_state, Hangman.tally(updated_state) }
   end
 
   # Helpers
@@ -35,11 +43,59 @@ defmodule Hangman.Game do
 
   # Returns the list of letters with unguessed letters
   # hidden with an underscore
+  #
+  # Note: The extra computation can be saved by storing the word and
+  #       letters separately then updating letters with each guess but
+  #       this isn't an expensive operation and seems more functional
   defp tally_letters(letters, used) do
     letters
-    |> Enum.map(&guessed_letter(&1, Enum.member?(used, &1)))
+    |> Enum.map(&guessed_correct_letter(&1, Enum.member?(used, &1)))
   end
 
-  defp guessed_letter(letter, true), do: letter
-  defp guessed_letter(_, false),     do: "_"
+  defp guessed_correct_letter(letter, true), do: letter
+  defp guessed_correct_letter(_, _),         do: "_"
+
+  # Updates the game state to include the latest guess
+  defp update_guesses(game = %Hangman.Game.State{}, guess) do
+    %Hangman.Game.State{ game |
+      used: game.used
+        |> MapSet.new()
+        |> MapSet.put(guess)
+        |> MapSet.to_list()
+        |> Enum.sort(),
+      last_guessed: guess
+    }
+  end
+
+  # Updates the game status based on the latest guess
+  defp update_status(game = %Hangman.Game.State{}, prev_guesses) do
+    %Hangman.Game.State{ game |
+      game_state: get_status(game, prev_guesses),
+      turns_left: game.turns_left - 1
+    }
+  end
+
+  defp get_status(game = %Hangman.Game.State{}, prev_guesses) do
+    check_status({
+      game.letters,
+      tally_letters(game.letters, game.used),
+      tally_letters(game.letters, prev_guesses),
+      game.used,
+      prev_guesses,
+      game.turns_left})
+  end
+
+  # Pattern matching to find the new game status,
+  # this should be refactored
+  #
+  # Won when the tally == letters
+  defp check_status({letters, letters, _, _, _, _}), do: :won
+  # Lost when only 1 turn is left(before deprecation)
+  defp check_status({_, _, _, _, _, 1}),             do: :lost
+  # Already used if the current and prev used lists are the same
+  defp check_status({_, _, _, used, used, _}),       do: :already_used
+  # Bad guess if the tally hasn't changed
+  defp check_status({_, tally, tally, _, _, _}),     do: :bad_guess
+  # Good guess otherwise
+  defp check_status(_),                              do: :good_guess
 end
--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -3,7 +3,7 @@ defmodule HangmanTest do
   doctest Hangman
 
   test "create new game" do
-    assert Hangman.new_game()
+    assert Hangman.new_game().letters != []
   end
 
   test "tally hides unguessed letters" do
@@ -16,5 +16,29 @@ defmodule HangmanTest do
     }
 
     assert Hangman.tally(game).letters == ["e", "_", "_", "x", "_", "_"]
+  end
+
+  test "tally hides all letters initially" do
+    game = %Hangman.Game.State{
+      game_state:   :initializing,
+      turns_left:   7,
+      letters:      ["e", "l", "i", "x", "i", "r"],
+      used:         [],
+      last_guessed: ""
+    }
+
+    assert Hangman.tally(game).letters == ["_", "_", "_", "_", "_", "_"]
+  end
+
+  test "tally doesn't hide any letters after a win" do
+    game = %Hangman.Game.State{
+      game_state:   :won,
+      turns_left:   6,
+      letters:      ["e", "l", "i", "x", "i", "r"],
+      used:         ["e", "i", "l", "r", "x", "z"],
+      last_guessed: "e"
+    }
+
+    assert Hangman.tally(game).letters == ["e", "l", "i", "x", "i", "r"]
   end
 end
